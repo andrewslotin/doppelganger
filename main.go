a01eed053cff5a040bd5b26f15011fdc0854ac1b
Andrew Slotin <andrew.slotin@gmail.com>
2015-12-11 23:53:24 +0100

    Obtaing GitHub token from env variable

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 var (
@@ -18,6 +21,12 @@ func main() {
 
 	log.SetOutput(os.Stderr)
 	log.SetFlags(log.LstdFlags)
+
+	token := os.Getenv("DOPPELGANGER_GITHUB_TOKEN")
+	if token == "" {
+		fmt.Fprintln(os.Stderr, "Missing GitHub access token (set DOPPELGANGER_GITHUB_TOKEN environment variable)")
+		os.Exit(-1)
+	}
 
 	*addr = fmt.Sprintf("%s:%d", *addr, *port)
 	log.Printf("doppelganger is listening on %s", *addr)
