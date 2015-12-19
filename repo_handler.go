c8d7ad7118dcc3fcc4c37cd0c534c3b485ce42c1
Andrew Slotin <andrew.slotin@gmail.com>
2015-12-19 14:06:23 +0100

    Add new repository mirror page
    When mirrored repository is not found offer to create it.

--- repo_handler.go
+++ repo_handler.go
@@ -11,7 +11,8 @@ import (
 )
 
 var (
-	repoTemplate = template.Must(template.ParseFiles("templates/repo/show.html.template"))
+	repoTemplate      = template.Must(template.ParseFiles("templates/repo/show.html.template"))
+	newMirrorTemplate = template.Must(template.ParseFiles("templates/repo/mirror.html.template"))
 )
 
 type RepoHandler struct {
@@ -30,6 +31,8 @@ func (handler *RepoHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) 
 	switch req.Method {
 	case "GET":
 		handler.Show(w, repoName)
+	case "POST":
+		http.Error(w, "Not implemented", http.StatusNotImplemented)
 	default:
 		http.Error(w, "Not found", http.StatusNotFound)
 	}
@@ -41,12 +44,23 @@ func (handler *RepoHandler) Show(w http.ResponseWriter, repoName string) {
 	switch repo, err := handler.repositories.Get(repoName); err {
 	case nil:
 		if err := repoTemplate.Execute(w, repo); err != nil {
-			log.Printf("failed to render repo/show %s with laters commit from %q (%s)", repo.FullName, repo.Master, err)
+			log.Printf("failed to render repo/show %s with latest commit from %q (%s)", repo.FullName, repo.Master, err)
 		} else {
 			log.Printf("rendered repo/show %s with latest commit from %q [%s]", repo.FullName, repo.Master, time.Since(startTime))
 		}
-	case git.ErrorNotFound, git.ErrorNotMirrored:
+	case git.ErrorNotFound:
 		http.Error(w, fmt.Sprintf("No such repository %q", repoName), http.StatusNotFound)
+	case git.ErrorNotMirrored:
+		repo := &git.Repository{
+			FullName: repoName,
+			GitURL:   fmt.Sprintf("git@doppelganger.local:%s.git", repoName),
+		}
+
+		if err := newMirrorTemplate.Execute(w, repo); err != nil {
+			log.Printf("failed to render repo/mirror %s (%s)", repo.FullName, err)
+		} else {
+			log.Printf("rendered repo/mirror %s [%s]", repo.FullName, time.Since(startTime))
+		}
 	default:
 		log.Printf("failed to fetch %s (%s)", repoName, err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
