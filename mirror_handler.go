41e8a29e6e05a9bf85ecca5ad5331e71c6301df8
Andrew Slotin <andrew.slotin@gmail.com>
2015-12-20 00:43:33 +0100

    Extract repository mirroring into MirrorHandler.CreateMirror

--- mirror_handler.go
+++ mirror_handler.go
@@ -31,19 +31,23 @@ func (handler *MirrorHandler) ServeHTTP(w http.ResponseWriter, req *http.Request
 		return
 	}
 
-	repo, err := handler.githubRepos.Get(repoName)
-	switch err {
-	case nil:
-		if err := handler.mirroredRepos.Create(repo.FullName, repo.GitURL); err != nil {
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
-			return
-		}
+	if err := handler.CreateMirror(w, repoName); err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 
-		log.Printf("mirrored %s from %s [%s]", repo.FullName, repo.GitURL, time.Since(startTime))
-		http.Redirect(w, req, fmt.Sprintf("/mirrored?repo=%s", url.QueryEscape(repo.FullName)), http.StatusSeeOther)
+	log.Printf("mirrored %s [%s]", repoName, time.Since(startTime))
+	http.Redirect(w, req, fmt.Sprintf("/mirrored?repo=%s", url.QueryEscape(repoName)), http.StatusSeeOther)
+}
+
+func (handler *MirrorHandler) CreateMirror(w http.ResponseWriter, repoName string) error {
+	switch repo, err := handler.githubRepos.Get(repoName); err {
+	case nil:
+		return handler.mirroredRepos.Create(repo.FullName, repo.GitURL)
 	case git.ErrorNotFound:
 		http.Error(w, "Source repository not found", http.StatusNotFound)
+		return nil
 	default:
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return err
 	}
 }
