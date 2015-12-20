f5c571da65bbab2b6e4f8c9239ac2614050abaf6
Andrew Slotin <andrew.slotin@gmail.com>
2015-12-20 01:01:29 +0100

    Add MirrorHandler.UpdateMirror

--- mirror_handler.go
+++ mirror_handler.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/andrewslotin/doppelganger/git"
@@ -39,6 +40,13 @@ func (handler *MirrorHandler) ServeHTTP(w http.ResponseWriter, req *http.Request
 		}
 
 		log.Printf("mirrored %s [%s]", repoName, time.Since(startTime))
+	case "update":
+		if err := handler.UpdateMirror(w, repoName); err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		log.Printf("updated mirror %s [%s]", repoName, time.Since(startTime))
 	default:
 		http.Error(w, fmt.Sprintf("Unsupported action %q", action), http.StatusBadRequest)
 		return
@@ -53,6 +61,18 @@ func (handler *MirrorHandler) CreateMirror(w http.ResponseWriter, repoName strin
 		return handler.mirroredRepos.Create(repo.FullName, repo.GitURL)
 	case git.ErrorNotFound:
 		http.Error(w, "Source repository not found", http.StatusNotFound)
+		return nil
+	default:
+		return err
+	}
+}
+
+func (handler *MirrorHandler) UpdateMirror(w http.ResponseWriter, repoName string) error {
+	switch repo, err := handler.githubRepos.Get(repoName); err {
+	case nil:
+		return handler.mirroredRepos.Update(repo.FullName)
+	case git.ErrorNotMirrored:
+		http.Error(w, "Source repository not mirrored", http.StatusNotFound)
 		return nil
 	default:
 		return err
