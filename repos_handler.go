55a97201a6944540d6a914324238d54d58805256
Andrew Slotin <andrew.slotin@gmail.com>
2016-04-09 23:13:56 +0200

    Fix request logging in repos handler

--- repos_handler.go
+++ repos_handler.go
@@ -33,7 +33,7 @@ func (handler *ReposHandler) ServeHTTP(w http.ResponseWriter, req *http.Request)
 
 	repos, err := handler.repositories.All()
 	if err != nil {
-		log.Printf("failed to get repos (%s) %s", err, req)
+		log.Printf("failed to get repos (%s) %v", err, req)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
