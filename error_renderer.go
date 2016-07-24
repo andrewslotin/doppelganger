d59a1e14856220c0b01bd5067fa42e11ed97c977
Andrew Slotin <andrew.slotin@gmail.com>
2016-07-24 02:00:38 +0200

    Add UserError type

--- error_renderer.go
+++ error_renderer.go
@@ -9,6 +9,18 @@ var (
 	internalErrorTemplate = template.Must(template.ParseFiles("templates/layout.html.template", "templates/errors/internal_error.html.template"))
 )
 
+// UserError wraps an internal server error and replaces its message with a
+// user-friendly version, while keeping the original error for inspection.
+type UserError struct {
+	Message       string
+	OriginalError error
+}
+
+// Error returns the message that is being sent back to user.
+func (e UserError) Error() string {
+	return e.Message
+}
+
 // WriteErrorPage renders an internal server error in a user-friendly way.
 func WriteErrorPage(w http.ResponseWriter, err error, status int) {
 	w.Header().Set("Content-Type", "text/html")
