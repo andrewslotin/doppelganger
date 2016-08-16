26ca9aa22b227aa311e49e7c2e201c2506a4df47
Andrew Slotin <andrew.slotin@gmail.com>
2016-08-16 14:17:18 +0200

    Add descriptions to all git.systemGit public methods

--- git/system_git.go
+++ git/system_git.go
@@ -47,21 +47,22 @@ func (gitCmd systemGit) Exec(path, command string, args ...string) (output []byt
 	return output, err
 }
 
-func (gitCmd systemGit) IsRepository(fullPath string) bool {
-	if fileInfo, err := os.Stat(fullPath); err != nil {
+// IsRepository checks if there if `path` is a git repository.
+func (gitCmd systemGit) IsRepository(path string) bool {
+	if fileInfo, err := os.Stat(path); err != nil {
 		if os.IsNotExist(err) {
 			return false
 		}
 
-		log.Printf("[WARN] failed to stat %s (%s)", fullPath, err)
+		log.Printf("[WARN] failed to stat %s (%s)", path, err)
 		return false
 	} else if !fileInfo.IsDir() {
 		return false
 	}
 
-	output, err := gitCmd.Exec(fullPath, "rev-parse", "--is-inside-git-dir")
+	output, err := gitCmd.Exec(path, "rev-parse", "--is-inside-git-dir")
 	if err != nil {
-		log.Printf("[WARN] git rev-parse --is-inside-git-dir returned error %s for %s (%s)", err, fullPath, string(output))
+		log.Printf("[WARN] git rev-parse --is-inside-git-dir returned error %s for %s (%s)", err, path, string(output))
 		return false
 	}
 
@@ -71,70 +72,74 @@ func (gitCmd systemGit) IsRepository(fullPath string) bool {
 	case "false":
 		return false
 	default:
-		log.Printf("[WARN] git rev-parse --is-inside-git-dir returned unexpected output for %s: %q", fullPath, string(output))
+		log.Printf("[WARN] git rev-parse --is-inside-git-dir returned unexpected output for %s: %q", path, string(output))
 		return false
 	}
 }
 
-func (gitCmd systemGit) CurrentBranch(fullPath string) string {
-	refName, err := gitCmd.Exec(fullPath, "symbolic-ref", "HEAD")
+// CurrentBranch returns the name of current branch in `path`.
+func (gitCmd systemGit) CurrentBranch(path string) string {
+	refName, err := gitCmd.Exec(path, "symbolic-ref", "HEAD")
 	if err != nil {
-		log.Printf("[WARN] git symbolic-ref HEAD returned error %s for %s (%s)", err, fullPath, string(refName))
+		log.Printf("[WARN] git symbolic-ref HEAD returned error %s for %s (%s)", err, path, string(refName))
 		return DefaultMaster
 	}
 
 	if !bytes.HasPrefix(refName, []byte("refs/heads/")) {
-		log.Printf("[WARN] unexpected reference name for %s (%q)", fullPath, refName)
+		log.Printf("[WARN] unexpected reference name for %s (%q)", path, refName)
 		return DefaultMaster
 	}
 
 	return string(bytes.TrimPrefix(refName, []byte("refs/heads/")))
 }
 
-func (gitCmd systemGit) LastCommit(fullPath string) (commit Commit, err error) {
-	output, err := gitCmd.Exec(fullPath, "log", "-n", "1", "--pretty="+gitPrettyFormat, "--date="+gitDateFormat)
+// LastCommit returns the latest commit from `path`.
+func (gitCmd systemGit) LastCommit(path string) (commit Commit, err error) {
+	output, err := gitCmd.Exec(path, "log", "-n", "1", "--pretty="+gitPrettyFormat, "--date="+gitDateFormat)
 	if err != nil {
-		log.Printf("[WARN] git log returned error %s for %s (%s)", err, fullPath, string(output))
+		log.Printf("[WARN] git log returned error %s for %s (%s)", err, path, string(output))
 		return commit, nil
 	}
 
 	lines := strings.SplitN(string(output), "\n", gitPrettyFormatFieldsNum)
 	if len(lines) < gitPrettyFormatFieldsNum {
-		log.Printf("[WARN] unexpected output from git log for %s (%s)", fullPath, string(output))
+		log.Printf("[WARN] unexpected output from git log for %s (%s)", path, string(output))
 		return commit, nil
 	}
 
 	commit.SHA, commit.Author, commit.Message = lines[0], lines[1], lines[3]
 	commit.Date, err = time.Parse(GitCommandDateLayout, lines[2])
 	if err != nil {
-		log.Printf("[WARN] unexpected date format from git log for %s (%s)", fullPath, lines[2])
+		log.Printf("[WARN] unexpected date format from git log for %s (%s)", path, lines[2])
 		commit.Date = time.Time{}
 	}
 
 	return commit, nil
 }
 
-func (gitCmd systemGit) CloneMirror(gitURL, fullPath string) error {
-	path, projectName := filepath.Dir(fullPath), filepath.Base(fullPath)
+// CloneMirror performs mirror clone of specified git URL to `path`.
+func (gitCmd systemGit) CloneMirror(gitURL, path string) error {
+	dir, projectName := filepath.Dir(path), filepath.Base(path)
 
-	if err := os.MkdirAll(path, 0755); err != nil {
-		log.Printf("failed to create %s (%s)", path, err)
-		return fmt.Errorf("failed to clone %s to %s", gitURL, fullPath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		log.Printf("failed to create %s (%s)", dir, err)
+		return fmt.Errorf("failed to clone %s to %s", gitURL, path)
 	}
 
-	output, err := gitCmd.Exec(path, "clone", "--mirror", gitURL, projectName)
+	output, err := gitCmd.Exec(dir, "clone", "--mirror", gitURL, projectName)
 	if err != nil {
-		log.Printf("git clone --mirror %s to %s returned %s (%s)", gitURL, fullPath, err, string(output))
-		return fmt.Errorf("failed to clone %s to %s", gitURL, fullPath)
+		log.Printf("git clone --mirror %s to %s returned %s (%s)", gitURL, path, err, string(output))
+		return fmt.Errorf("failed to clone %s to %s", gitURL, path)
 	}
 
 	return nil
 }
 
-func (gitCmd systemGit) UpdateRemote(fullPath string) error {
-	output, err := gitCmd.Exec(fullPath, "remote", "update")
+// UpdateRemote does `git remote update` in specified `path`.
+func (gitCmd systemGit) UpdateRemote(path string) error {
+	output, err := gitCmd.Exec(path, "remote", "update")
 	if err != nil {
-		log.Printf("[WARN] git remote update returned error %s for %s (%s)", err, fullPath, string(output))
+		log.Printf("[WARN] git remote update returned error %s for %s (%s)", err, path, string(output))
 		return errors.New("update failed")
 	}
 
