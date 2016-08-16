50c5557894abdd81c716955775500ebfb383e861
Andrew Slotin <andrew.slotin@gmail.com>
2016-08-16 12:28:53 +0200

    Add Committer to git.Commit

--- git/commit.go
+++ git/commit.go
@@ -4,8 +4,9 @@ import "time"
 
 // Commit represents a single commit in Git repository.
 type Commit struct {
-	SHA     string
-	Message string
-	Author  string
-	Date    time.Time
+	SHA       string
+	Message   string
+	Author    string
+	Committer string
+	Date      time.Time
 }
